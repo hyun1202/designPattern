f1fbc30a43959f42efb27454da55f64bd1829d29
hyun <apfhd0257@naver.com>
2024-10-21 16:07:28 +0900

    [싱글톤 패턴]: 멀티 스레딩 문제 해결 - 2. 처음부터 인스턴스 생성

--- src/ch5/Singleton.java
+++ src/ch5/Singleton.java
@@ -1,18 +1,14 @@
 package ch5;
 
 public class Singleton {
-    private static Singleton instance;
+    // 2. 처음부터 인스턴스 생성
+    private static Singleton instance = new Singleton();
 
     private Singleton() {
 
     }
 
-    // 1. 멀티 스레딩 문제 해결 - 동기화 사용
-    // 단점: 성능 저하
-    public static synchronized Singleton getInstance() {
-        if (instance == null) {
-            instance = new Singleton();
-        }
+    public static Singleton getInstance() {
         return instance;
     }
 }
