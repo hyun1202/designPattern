48f8ab76d4c7739bed5988cb1296430362ab7136
hyun <apfhd0257@naver.com>
2024-10-21 18:22:50 +0900

    [커맨드 패턴]: 여러개의 기능이 있는 리모컨 - 람다를 이용한 setCommand 리팩토링

--- src/ch6/remoteController/RemoteController.java
+++ src/ch6/remoteController/RemoteController.java
@@ -5,20 +5,28 @@ public class RemoteController {
         RemoteControl remote = new RemoteControl();
         Light light = new Light("Living Room");
         Light light2 = new Light("Kitchen");
-        LightOnCommand livingRoomLightOn = new LightOnCommand(light);
-        LightOnCommand kitchenLightOn = new LightOnCommand(light2);
+//        LightOnCommand livingRoomLightOn = new LightOnCommand(light);
+//        LightOnCommand kitchenLightOn = new LightOnCommand(light2);
 
-        LightOffCommand livingRoomLightOff = new LightOffCommand(light);
-        LightOffCommand kitchenLightOff = new LightOffCommand(light2);
+//        LightOffCommand livingRoomLightOff = new LightOffCommand(light);
+//        LightOffCommand kitchenLightOff = new LightOffCommand(light2);
 
         Stereo stereo = new Stereo();
 
-        StereoOnWithCDCommand stereoOn = new StereoOnWithCDCommand(stereo);
-        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+//        StereoOnWithCDCommand stereoOn = new StereoOnWithCDCommand(stereo);
+//        StereoOffCommand stereoOff = new StereoOffCommand(stereo);
 
-        remote.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-        remote.setCommand(1, kitchenLightOn, kitchenLightOff);
-        remote.setCommand(2, stereoOn, stereoOff);
+        // Lambda 표현식으로 변경
+        // 추상 메소드가 하나일 때만 사용 가능하다.
+        // () -> light.on()을 light::on으로 변경 가능
+        // remote.setCommand(0, () -> light.on(), () -> light.off());
+        remote.setCommand(0, light::on, light::off);
+        remote.setCommand(1, light2::on, light2::off);
+        remote.setCommand(2, () -> {
+            stereo.on();
+            stereo.setCd();
+            stereo.setVolume(11);
+        }, stereo::off);
 
         System.out.println(remote);
 
