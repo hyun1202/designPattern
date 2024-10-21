7534dee79681a456cdd7ee8c519f93291fb93b45
hyun <apfhd0257@naver.com>
2024-10-21 16:11:19 +0900

    [싱글톤 패턴]: 멀티 스레딩 문제 해결 - 3. Double-Checked Locking 사용

--- src/ch5/Singleton.java
+++ src/ch5/Singleton.java
@@ -1,14 +1,24 @@
 package ch5;
 
 public class Singleton {
-    // 2. 처음부터 인스턴스 생성
-    private static Singleton instance = new Singleton();
+    // volatile 키워드를 사용
+    // cache에 저장된 값이 아닌 Main Memory에서 읽어온다.
+    private volatile static Singleton instance;
 
     private Singleton() {
 
     }
 
+    // 3. Double-Checked Locking 사용
+    // 인스턴스가 생성되어있는지 확인 후 생성되어 있지 않으면 동기화
     public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
         return instance;
     }
 }
