e4201fac59efebccb27ac9bd728750da5f827d4e
hyun <apfhd0257@naver.com>
2024-11-24 12:10:13 +0900

    [복합 패턴-컴포지트]: 5. 오리 무리 관리2

--- src/ch12/DuckSimulator.java
+++ src/ch12/DuckSimulator.java
@@ -1,6 +1,7 @@
 package ch12;
 
 import ch12.adapter.GooseAdapter;
+import ch12.composite.Flock;
 import ch12.decorator.QuackCount;
 import ch12.factory.AbstractDuckFactory;
 import ch12.factory.CountingDuckFactory;
@@ -17,15 +18,36 @@ public class DuckSimulator {
         Quackable redheadDuck = duckFactory.createRedheadDuck();
         Quackable duckCall = duckFactory.createDuckCall();
         Quackable rubberDuck = duckFactory.createRubberDuck();
-        Quackable gooseAdapter = new GooseAdapter(new Goose());
+        Quackable gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("\n 오리 시뮬레이션 게임");
+        System.out.println("\n 오리 시뮬레이션 게임: 무리 (+컴포지트)");
 
-        simulate(mallardDuck);
-        simulate(redheadDuck);
-        simulate(duckCall);
-        simulate(rubberDuck);
-        simulate(gooseAdapter);
+        Flock flockOfDucks = new Flock();
+
+        flockOfDucks.add(redheadDuck);
+        flockOfDucks.add(duckCall);
+        flockOfDucks.add(rubberDuck);
+        flockOfDucks.add(gooseDuck);
+
+        Flock flockOfMallards = new Flock();
+
+        Quackable mallardOne = duckFactory.createMallardDuck();
+        Quackable mallardTwo = duckFactory.createMallardDuck();
+        Quackable mallardThree = duckFactory.createMallardDuck();
+        Quackable mallardFour = duckFactory.createMallardDuck();
+
+        flockOfMallards.add(mallardOne);
+        flockOfMallards.add(mallardTwo);
+        flockOfMallards.add(mallardThree);
+        flockOfMallards.add(mallardFour);
+
+        flockOfDucks.add(flockOfMallards);
+
+        System.out.println("\n 오리 시뮬레이션 게임: 전체 무리");
+        simulate(flockOfDucks);
+
+        System.out.println("\n 오리 시뮬레이션 게임: 물 오리 무리");
+        simulate(flockOfMallards);
 
         System.out.println("오리가 소리 낸 횟수: " +
                 QuackCount.getQuacks() + " 번");
